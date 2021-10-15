5d3127bc98f3d0ca1f2a8f25c2aea8379211bebd
allyboy08 <alexsassman65@gmail.com>
2021-10-15 13:50:26 +0200

    update

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{score}} ms</p>
+  <p class="rank"> {{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Yellow flash'
+        } else if (this.score < 400) {
+            this.rank = 'Usain bolt'
+        } else {
+            this.rank = 'Your weak'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
